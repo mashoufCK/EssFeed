3ee8adea767cff33af58b9eea0e0ac9cbde0c706
MC <mashuf.ir@gmail.com>
2023-06-15 00:07:27 -0500

    Initial Commit

---
+++ EssFeed/EssFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssFeed.h
+//  EssFeed
+//
+//  Created by Mashuf Chowdhury on 6/15/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssFeed.
+FOUNDATION_EXPORT double EssFeedVersionNumber;
+
+//! Project version string for EssFeed.
+FOUNDATION_EXPORT const unsigned char EssFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssFeed/PublicHeader.h>
+
+

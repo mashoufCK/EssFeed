04c5e32a6a15fec802f0e83cd031c5fb8cba6e48
MC <mashuf.ir@gmail.com>
2023-06-17 06:10:02 -0500

    Create FeedItem model

---
+++ EssFeed/FeedItem.swift
@@ -0,0 +1,19 @@
+//
+//  FeedItem.swift
+//  EssFeed
+//
+//  Created by Mashuf Chowdhury on 6/17/23.
+//
+
+import Foundation
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+    
+}
+

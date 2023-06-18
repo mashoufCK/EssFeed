e1112dfbfe248e5994399735d992dc819fe79f4b
MC <mashuf.ir@gmail.com>
2023-06-18 14:40:26 -0500

    Request data from URL on load

--- EssFeedTests/RemoteFeedLoaderTests.swift
+++ EssFeedTests/RemoteFeedLoaderTests.swift
@@ -8,17 +8,25 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+ 
+    func load(){
+        HTTPClient.shared.requestedURL = URL(string:  "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
-class RemoteFeedLoaderTests: XCTest {
+class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         //sut.load()
@@ -26,4 +34,13 @@ class RemoteFeedLoaderTests: XCTest {
         XCTAssertNil(client.requestedURL )
     }
     
+    func test_load_requestDataFromURL() {
+        
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
 }

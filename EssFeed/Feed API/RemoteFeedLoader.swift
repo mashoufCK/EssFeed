a3be4c0ee93a32ae8b7e8dbbf54618113d64e53d
MC <mashuf.ir@gmail.com>
2023-06-22 15:23:31 -0500

    Use explicit JSON data for non 200 HTTP response  test

--- EssFeed/Feed API/RemoteFeedLoader.swift
+++ EssFeed/Feed API/RemoteFeedLoader.swift
@@ -43,8 +43,9 @@ public final class RemoteFeedLoader {
             result  in
             
             switch result {
-            case let .success(data, _):
-                if let root = try?
+            case let .success(data, response):
+                if response.statusCode == 200,
+                let root = try?
                     
                     JSONDecoder().decode(Root.self, from: data) {
                     

e6382aa93be1acdca97aa78c2614dbf9e83e3f05
MC <mashuf.ir@gmail.com>
2023-06-26 16:54:37 -0500

    move URL creation to a shared factory method

--- EssFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -42,7 +42,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_performsGETRequestsWihURL() {
         
-        let url = URL(string: "http://any-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
         URLProtocolStub.observeRequest {request in
             XCTAssertEqual(request.url, url)
@@ -50,19 +50,19 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        makeSUT().get (from: url) { _ in }
+        makeSUT().get (from: anyURL()) { _ in }
         wait(for: [exp], timeout: 1.0)
 
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "http://any-url.com")!
+       
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil,  error: error)
                 
         let exp  = expectation(description: "Wait for completion")
         
-        makeSUT().get(from: url) {result in
+        makeSUT().get(from: anyURL()) {result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError, error)
@@ -81,6 +81,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func anyURL() -> URL{
+      return  URL(string: "http://any-url.com")!
     }
     
     private class URLProtocolStub: URLProtocol {
